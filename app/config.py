b9f44c1d62a83ad96a0d7e0dc28bb4341feba394
quick-sort@outlook.com <quick-sort@outlook.com>
2024-07-27 23:34:55 +0800

    update

--- app/config.py
+++ app/config.py
@@ -1,9 +1,15 @@
+import os
+import logging
 from typing import Literal, Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import PostgresDsn
+
+logger = logging.getLogger(__name__)
+env_file = os.getenv('ELASTIC_SPIDER_ENV_FILE', '.env')
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env',
+        env_file=env_file,
         validate_default=False,
     )
     ENV: str = 'dev'
@@ -17,3 +23,13 @@ class Settings(BaseSettings):
     SQLALCHEMY_URI: PostgresDsn = 'postgresql+asyncpg://postgres:postgres@localhost:5432/app'
 
 settings = Settings()
+
+logging.basicConfig(
+    format='%(levelname)s:%(asctime)s - %(pathname)s:%(lineno)d:%(funcName)s %(message)s',
+    encoding='utf-8',
+    level=getattr(logging, settings.LOG_LEVEL.upper())
+)
+if os.path.exists(env_file):
+    logger.info('loading settings from %s', env_file)
+else:
+    logger.info('loading settings from environments')
