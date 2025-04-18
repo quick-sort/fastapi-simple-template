1c8044b61625ed3a98f98c39130a72d4232599c1
quick-sort@outlook.com <quick-sort@outlook.com>
2025-04-18 21:56:31 +0800

    add static files

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.config import settings
 from app.api.endpoints import router as api_router
 from app.api.middlewares import config_middlewares
+from app.static import setup_static
 
 app = FastAPI(
     debug=True if settings.ENV != 'prod' else False,
@@ -9,3 +10,4 @@ app = FastAPI(
 )
 app.include_router(api_router, prefix='/api')
 config_middlewares(app)
+setup_static(app)
--- app/static.py
+++ app/static.py
@@ -1,14 +1,27 @@
 import os
+import mimetypes
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException, StarletteHTTPException
 from app.config import settings
 
 router = APIRouter()
+mimetypes.add_type("text/javascript", ".js")
+
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope):
+        try:
+            return await super().get_response(path, scope)
+        except (HTTPException, StarletteHTTPException) as ex:
+            if ex.status_code == 404:
+                if path.endswith(".js"):
+                    # Return 404 for javascript files
+                    raise ex
+                else:
+                    return await super().get_response("index.html", scope)
+            else:
+                raise ex
+
 def setup_static(app: FastAPI):
     if os.path.exists(settings.STATIC_FILES_DIR):
-        app.mount("/", StaticFiles(directory=settings.STATIC_FILES_DIR), name="static")
-
-@router.get("/")
-async def root():
-    return FileResponse(os.path.join(settings.STATIC_FILES_DIR, 'index.html'))
+        app.mount("/", SPAStaticFiles(directory=settings.STATIC_FILES_DIR), name="static")
